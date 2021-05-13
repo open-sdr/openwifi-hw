9e6e52df5f5589810bccaeb984d32571409cd180
Xianjun Jiao <xianjun.jiao@ugent.be>
2021-05-13 17:39:33 +0200

    CSI fuzzer feature

--- ip_repo/low/tx_intf/src/tx_iq_intf.v
+++ ip_repo/low/tx_intf/src/tx_iq_intf.v
@@ -5,6 +5,7 @@
 	module tx_iq_intf #
 	(
     parameter integer C_S00_AXIS_TDATA_WIDTH	= 64,
+    parameter integer CSI_FUZZER_WIDTH = 6,
     parameter integer IQ_DATA_WIDTH = 16
 	)
 	(
@@ -12,29 +13,32 @@
 	  input wire clk,
 	    
 	  output wire [(2*IQ_DATA_WIDTH-1):0] wifi_iq_pack,
-      input  wire wifi_iq_ready,
-      output wire wifi_iq_valid,
+    input  wire wifi_iq_ready,
+    output wire wifi_iq_valid,
       
-      input wire [9:0] tx_hold_threshold,
-      input wire signed [9:0] bb_gain,
+    input wire [9:0] tx_hold_threshold,
+    input wire signed [9:0] bb_gain,
+    input wire signed [(CSI_FUZZER_WIDTH-1):0] bb_gain1,
+    input wire              bb_gain1_rot90_flag,
+    input wire signed [(CSI_FUZZER_WIDTH-1):0] bb_gain2,
+    input wire              bb_gain2_rot90_flag,
 
 	  input wire signed [(IQ_DATA_WIDTH-1) : 0] rf_i,
-      input wire signed [(IQ_DATA_WIDTH-1) : 0] rf_q,
-      input wire rf_iq_valid,
+    input wire signed [(IQ_DATA_WIDTH-1) : 0] rf_q,
+    input wire rf_iq_valid,
 
-      // to lbt
-      output wire tx_iq_fifo_empty,
+    // to lbt
+    output wire tx_iq_fifo_empty,
 
-      // to tx core
-      output wire tx_hold
+    // to tx core
+    output wire tx_hold
 	);
   
     reg signed [(10+IQ_DATA_WIDTH-1) : 0] rf_i_tmp;
     reg signed [(10+IQ_DATA_WIDTH-1) : 0] rf_q_tmp;
     reg tx_iq_fifo_wren;
 
-    wire [(2*IQ_DATA_WIDTH-1):0] tx_iq_fifo_out_merge;
-    wire [(2*IQ_DATA_WIDTH-1):0] tx_iq_fifo_out_merge_ch_sel;
+    wire [(2*IQ_DATA_WIDTH-1):0] tx_iq_fifo_out_zero_padding;
     wire [(2*IQ_DATA_WIDTH-1):0] tx_iq_fifo_out;
     wire [(2*IQ_DATA_WIDTH-1):0] tx_iq_fifo_in;
     wire tx_iq_fifo_rden;
@@ -48,9 +52,7 @@
     assign tx_hold = (data_count>tx_hold_threshold?1:0);
 
     //output mux
-    assign tx_iq_fifo_out_merge[(2*IQ_DATA_WIDTH-1):0] = (tx_iq_fifo_empty==1?0:tx_iq_fifo_out);
-    assign tx_iq_fifo_out_merge_ch_sel = tx_iq_fifo_out_merge;
-    assign wifi_iq_pack = tx_iq_fifo_out_merge_ch_sel;
+    assign tx_iq_fifo_out_zero_padding = (tx_iq_fifo_empty==1?0:tx_iq_fifo_out);
     assign wifi_iq_valid = 1;
 
     //fifo rd mux
@@ -59,6 +61,7 @@
     //fifo in mux
     assign tx_iq_fifo_in = {rf_q_tmp[(7+IQ_DATA_WIDTH-1):7], rf_i_tmp[(7+IQ_DATA_WIDTH-1):7]};
 
+    // gain module
     always @(posedge clk)                                              
     begin
       if (!rstn) begin                                                                    
@@ -70,7 +73,29 @@
           rf_q_tmp<=rf_q*bb_gain;
           tx_iq_fifo_wren <= (~tx_hold&rf_iq_valid);
       end
-    end   
+    end
+
+    //csi fuzzer at fifo out
+    csi_fuzzer # (
+        .CSI_FUZZER_WIDTH(CSI_FUZZER_WIDTH),
+        .IQ_DATA_WIDTH(IQ_DATA_WIDTH)
+    ) csi_fuzzer_i (
+        .rstn(rstn),
+        .clk(clk),
+
+        // input data
+        .iq(tx_iq_fifo_out_zero_padding),
+        .iq_valid(wifi_iq_ready),
+
+        // FIR coef of the fuzzer
+        .bb_gain1(bb_gain1),
+        .bb_gain1_rot90_flag(bb_gain1_rot90_flag),
+        .bb_gain2(bb_gain2),
+        .bb_gain2_rot90_flag(bb_gain2_rot90_flag),
+
+        // output data
+        .iq_out(wifi_iq_pack)
+    );
           
     // fifo32_1clk_dep512 fifo32_1clk_dep512_i (
     //     .CLK(clk),
