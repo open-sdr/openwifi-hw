9a9fdef9cca9ce970df15da2d2087e09760b7608
weiliu <wei.liu1011@gmail.com>
2022-03-25 14:30:00 +0100

    add tsf port

--- ip/tx_intf/src/tx_intf.v
+++ ip/tx_intf/src/tx_intf.v
@@ -140,7 +140,8 @@
 		input wire [C_S00_AXIS_TDATA_WIDTH-1 : 0] s00_axis_tdata,
 		input wire [(C_S00_AXIS_TDATA_WIDTH/8)-1 : 0] s00_axis_tstrb,
 		input wire  s00_axis_tlast,
-		input wire  s00_axis_tvalid
+		input wire  s00_axis_tvalid,
+        	input wire [63:0] tsf_runtime_val
 	);
 
 	function integer clogb2 (input integer bit_depth);                                   
