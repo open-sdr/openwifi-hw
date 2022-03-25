289158ea0056c65aa63f7189161a9951cb6ae6e1
Xianjun Jiao <xianjun.jiao@ugent.be>
2022-03-25 09:03:34 +0100

    Use the more efficinet dual channel moving average module for iq_abs_avg.v

--- ip/xpu/src/iq_abs_avg.v
+++ ip/xpu/src/iq_abs_avg.v
@@ -26,14 +26,17 @@
     wire signed [(IQ_DATA_WIDTH-1):0] i_abs;
     wire signed [(IQ_DATA_WIDTH-1):0] q_abs;
 
-    wire signed [(2*IQ_DATA_WIDTH-1):0] iq_abs_mv_avg;
+    // wire signed [(2*IQ_DATA_WIDTH-1):0] iq_abs_mv_avg;
+    wire signed [(IQ_DATA_WIDTH-1):0] i_abs_mv_avg;
+    wire signed [(IQ_DATA_WIDTH-1):0] q_abs_mv_avg;
     
     wire iq_abs_avg_ready_internal;
     
     assign i_abs = (i_dc_rm[IQ_DATA_WIDTH-1]==1'b1)?(-i_dc_rm):i_dc_rm;
     assign q_abs = (q_dc_rm[IQ_DATA_WIDTH-1]==1'b1)?(-q_dc_rm):q_dc_rm;
     
-    assign iq_rssi = ((iq_abs_mv_avg[(IQ_DATA_WIDTH-1):0]+iq_abs_mv_avg[(2*IQ_DATA_WIDTH-1):IQ_DATA_WIDTH])>>1);
+    // assign iq_rssi = ((iq_abs_mv_avg[(IQ_DATA_WIDTH-1):0]+iq_abs_mv_avg[(2*IQ_DATA_WIDTH-1):IQ_DATA_WIDTH])>>1);
+    assign iq_rssi = ((i_abs_mv_avg+q_abs_mv_avg)>>1);
 
     dc_rm # (
         .DATA_WIDTH(IQ_DATA_WIDTH)
@@ -48,15 +51,47 @@
         .iq_dc_rm_valid(iq_dc_rm_valid)
     );
 
-    mv_avg32 # (
-    ) mv_avg32_i (
-        .M_AXIS_DATA_tdata(iq_abs_mv_avg),
-        .M_AXIS_DATA_tvalid(iq_rssi_valid),
-        .S_AXIS_DATA_tdata({q_abs,i_abs}),
-        .S_AXIS_DATA_tready(iq_abs_avg_ready_internal),
-        .S_AXIS_DATA_tvalid(iq_dc_rm_valid),
-        .aclk(clk),
-        .aresetn(rstn)
+    // mv_avg32 # (
+    // ) mv_avg32_i (
+    //     .M_AXIS_DATA_tdata(iq_abs_mv_avg),
+    //     .M_AXIS_DATA_tvalid(iq_rssi_valid),
+    //     .S_AXIS_DATA_tdata({q_abs,i_abs}),
+    //     .S_AXIS_DATA_tready(iq_abs_avg_ready_internal),
+    //     .S_AXIS_DATA_tvalid(iq_dc_rm_valid),
+    //     .aclk(clk),
+    //     .aresetn(rstn)
+    // );
+
+    mv_avg_dual_ch #(.DATA_WIDTH0(IQ_DATA_WIDTH), .DATA_WIDTH1(IQ_DATA_WIDTH), .LOG2_AVG_LEN(5)) mv_avg32_dual_ch_inst (
+        .clk(clk),
+        .rstn(rstn),
+
+        .data_in0(i_abs),
+        .data_in1(q_abs),
+        .data_in_valid(iq_dc_rm_valid),
+
+        .data_out0(i_abs_mv_avg),
+        .data_out1(q_abs_mv_avg),
+        .data_out_valid(iq_rssi_valid)
     );
-      
+
+    // mv_avg #(.DATA_WIDTH(IQ_DATA_WIDTH), .LOG2_AVG_LEN(5)) mv_avg32_i_inst (
+    //     .clk(clk),
+    //     .rstn(rstn),
+
+    //     .data_in(i_abs),
+    //     .data_in_valid(iq_dc_rm_valid),
+    //     .data_out(i_abs_mv_avg),
+    //     .data_out_valid(iq_rssi_valid)
+    // );
+    // mv_avg #(.DATA_WIDTH(IQ_DATA_WIDTH), .LOG2_AVG_LEN(5)) mv_avg32_q_inst (
+    //     .clk(clk),
+    //     .rstn(rstn),
+
+    //     .data_in(q_abs),
+    //     .data_in_valid(iq_dc_rm_valid),
+    //     .data_out(q_abs_mv_avg),
+    //     .data_out_valid()
+    // );
+
 	endmodule
