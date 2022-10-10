4a07aae90cd5d6b5f8b9fd650f290fa384965174
black-pigeon <1530604142@qq.com>
2022-10-10 17:32:09 +0800

    Add support for antsdr_e200

--- boards/pack_hdf_bit.sh
+++ boards/pack_hdf_bit.sh
@@ -4,7 +4,7 @@
 # // SPDX-FileCopyrightText: 2022 UGent
 # // SPDX-License-Identifier: AGPL-3.0-or-later
 
-BOARD_NAME_ALL="antsdr sdrpi zc706_fmcs2 zed_fmcs2 zc702_fmcs2 adrv9361z7035 adrv9364z7020 zcu102_fmcs2"
+BOARD_NAME_ALL="antsdr antsdr_e200 sdrpi zc706_fmcs2 zed_fmcs2 zc702_fmcs2 adrv9361z7035 adrv9364z7020 zcu102_fmcs2"
 for BOARD_NAME in $BOARD_NAME_ALL
 do
     tar -zcvf $BOARD_NAME/sdk/system_top_hw_platform_0/hdf_and_bit.tar.gz $BOARD_NAME/sdk/system_top_hw_platform_0/system.hdf $BOARD_NAME/sdk/system_top_hw_platform_0/system_top.bit
