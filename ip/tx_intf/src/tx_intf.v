976563ecc671ebebda2ca4dbb14f1a027b790c9c
Xianjun Jiao <xianjun.jiao@ugent.be>
2023-01-16 14:47:08 +0100

    Minor formating

--- ip/tx_intf/src/tx_intf.v
+++ ip/tx_intf/src/tx_intf.v
@@ -49,105 +49,105 @@ module tx_intf #
   output wire [(DAC_PACK_DATA_WIDTH-1) : 0] dac_data,
   input  wire dac_ready,
 
-        // Ports to side_ch for check
-        output wire [(2*IQ_DATA_WIDTH-1) : 0] iq0_for_check,
-        output wire [(2*IQ_DATA_WIDTH-1) : 0] iq1_for_check,
-        output wire iq_valid_for_check,
+  // Ports to side_ch for check
+  output wire [(2*IQ_DATA_WIDTH-1) : 0] iq0_for_check,
+  output wire [(2*IQ_DATA_WIDTH-1) : 0] iq1_for_check,
+  output wire iq_valid_for_check,
 
-        // from openofdm rx
-        input  wire fcs_in_strobe,
+  // from openofdm rx
+  input  wire fcs_in_strobe,
 
-	    // Ports to ACC: PHY_TX
-	    output wire phy_tx_start,
-        output wire tx_hold,
-        input wire  [(WIFI_TX_BRAM_ADDR_WIDTH-1):0] bram_addr,
-        input wire signed [(IQ_DATA_WIDTH-1) : 0] rf_i_from_acc,
-	    input wire signed [(IQ_DATA_WIDTH-1) : 0] rf_q_from_acc,
-	    input wire rf_iq_valid_from_acc,
-	    output wire [(WIFI_TX_BRAM_DATA_WIDTH-1) : 0] data_to_acc,
-        output wire  [(WIFI_TX_BRAM_ADDR_WIDTH-1):0] bram_addr_to_xpu,
-        input wire tx_start_from_acc,
-        input wire tx_end_from_acc,
-    
-	    // interrupt to PS
-        output wire tx_itrpt,
+  // Ports to ACC: PHY_TX
+  output wire phy_tx_start,
+  output wire tx_hold,
+  input wire  [(WIFI_TX_BRAM_ADDR_WIDTH-1):0] bram_addr,
+  input wire signed [(IQ_DATA_WIDTH-1) : 0] rf_i_from_acc,
+  input wire signed [(IQ_DATA_WIDTH-1) : 0] rf_q_from_acc,
+  input wire rf_iq_valid_from_acc,
+  output wire [(WIFI_TX_BRAM_DATA_WIDTH-1) : 0] data_to_acc,
+  output wire  [(WIFI_TX_BRAM_ADDR_WIDTH-1):0] bram_addr_to_xpu,
+  input wire tx_start_from_acc,
+  input wire tx_end_from_acc,
+  
+  // interrupt to PS
+  output wire tx_itrpt,
 
-        // for led
-        output wire tx_itrpt_led,
-        output wire tx_end_led,
+  // for led
+  output wire tx_itrpt_led,
+  output wire tx_end_led,
 
-        // for xpu
-        input wire [79:0] tx_status,
-        input wire [47:0] mac_addr,
-        output wire [(WIFI_TX_BRAM_DATA_WIDTH-1):0] douta,//for changing some bits to indicate it is the 1st pkt or retransmitted pkt
-        // output wire [(IQ_DATA_WIDTH-1):0] i0,
-        // output wire [(IQ_DATA_WIDTH-1):0] q0,
-        // output wire [(IQ_DATA_WIDTH-1):0] i1,
-        // output wire [(IQ_DATA_WIDTH-1):0] q1,
-        // output wire iq_valid,
-        //output wire [31:0] mixer_cfg,
-        output wire tx_iq_fifo_empty,
-        //output wire [13:0] tx_iq_fifo_data_count,
-        input wire [3:0] slice_en, // allow sending new Linux packet or not
-        input wire backoff_done,
-        input wire tx_bb_is_ongoing,
-        input wire ack_tx_flag,
-        input wire wea_from_xpu,
-        input wire [9:0] addra_from_xpu,
-        input wire [(C_S00_AXIS_TDATA_WIDTH-1):0] dina_from_xpu,
-        output wire tx_pkt_need_ack,
-        output wire [3:0] tx_pkt_retrans_limit,
-        output wire use_ht_aggr,
-        input wire tx_try_complete,
-        input wire [9:0] num_slot_random,
-        input wire [3:0] cw,
-        input wire retrans_in_progress,
-        input wire start_retrans,
-        input wire start_tx_ack,
-        input wire tx_control_state_idle,
-        output wire cts_toself_bb_is_ongoing,
-        output wire cts_toself_rf_is_ongoing,
-        input wire tsf_pulse_1M,
-        input wire [3:0] band,
-        input wire [7:0] channel,
-        output wire quit_retrans,
-        output wire reset_backoff,
-        output wire high_trigger,
-        output wire [1:0] tx_queue_idx_to_xpu,
+  // for xpu
+  input wire [79:0] tx_status,
+  input wire [47:0] mac_addr,
+  output wire [(WIFI_TX_BRAM_DATA_WIDTH-1):0] douta,//for changing some bits to indicate it is the 1st pkt or retransmitted pkt
+  // output wire [(IQ_DATA_WIDTH-1):0] i0,
+  // output wire [(IQ_DATA_WIDTH-1):0] q0,
+  // output wire [(IQ_DATA_WIDTH-1):0] i1,
+  // output wire [(IQ_DATA_WIDTH-1):0] q1,
+  // output wire iq_valid,
+  //output wire [31:0] mixer_cfg,
+  output wire tx_iq_fifo_empty,
+  //output wire [13:0] tx_iq_fifo_data_count,
+  input wire [3:0] slice_en, // allow sending new Linux packet or not
+  input wire backoff_done,
+  input wire tx_bb_is_ongoing,
+  input wire ack_tx_flag,
+  input wire wea_from_xpu,
+  input wire [9:0] addra_from_xpu,
+  input wire [(C_S00_AXIS_TDATA_WIDTH-1):0] dina_from_xpu,
+  output wire tx_pkt_need_ack,
+  output wire [3:0] tx_pkt_retrans_limit,
+  output wire use_ht_aggr,
+  input wire tx_try_complete,
+  input wire [9:0] num_slot_random,
+  input wire [3:0] cw,
+  input wire retrans_in_progress,
+  input wire start_retrans,
+  input wire start_tx_ack,
+  input wire tx_control_state_idle,
+  output wire cts_toself_bb_is_ongoing,
+  output wire cts_toself_rf_is_ongoing,
+  input wire tsf_pulse_1M,
+  input wire [3:0] band,
+  input wire [7:0] channel,
+  output wire quit_retrans,
+  output wire reset_backoff,
+  output wire high_trigger,
+  output wire [1:0] tx_queue_idx_to_xpu,
 
-		// Ports of Axi Slave Bus Interface S00_AXI
-		input wire  s00_axi_aclk,
-		input wire  s00_axi_aresetn,
-		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
-		input wire [2 : 0] s00_axi_awprot,
-		input wire  s00_axi_awvalid,
-		output wire  s00_axi_awready,
-		input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
-		input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
-		input wire  s00_axi_wvalid,
-		output wire  s00_axi_wready,
-		output wire [1 : 0] s00_axi_bresp,
-		output wire  s00_axi_bvalid,
-		input wire  s00_axi_bready,
-		input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
-		input wire [2 : 0] s00_axi_arprot,
-		input wire  s00_axi_arvalid,
-		output wire  s00_axi_arready,
-		output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
-		output wire [1 : 0] s00_axi_rresp,
-		output wire  s00_axi_rvalid,
-		input wire  s00_axi_rready,
+  // Ports of Axi Slave Bus Interface S00_AXI
+  input wire  s00_axi_aclk,
+  input wire  s00_axi_aresetn,
+  input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_awaddr,
+  input wire [2 : 0] s00_axi_awprot,
+  input wire  s00_axi_awvalid,
+  output wire  s00_axi_awready,
+  input wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_wdata,
+  input wire [(C_S00_AXI_DATA_WIDTH/8)-1 : 0] s00_axi_wstrb,
+  input wire  s00_axi_wvalid,
+  output wire  s00_axi_wready,
+  output wire [1 : 0] s00_axi_bresp,
+  output wire  s00_axi_bvalid,
+  input wire  s00_axi_bready,
+  input wire [C_S00_AXI_ADDR_WIDTH-1 : 0] s00_axi_araddr,
+  input wire [2 : 0] s00_axi_arprot,
+  input wire  s00_axi_arvalid,
+  output wire  s00_axi_arready,
+  output wire [C_S00_AXI_DATA_WIDTH-1 : 0] s00_axi_rdata,
+  output wire [1 : 0] s00_axi_rresp,
+  output wire  s00_axi_rvalid,
+  input wire  s00_axi_rready,
 
-		// Ports of Axi Slave Bus Interface S00_AXIS to PS
-		input wire  s00_axis_aclk,
-		input wire  s00_axis_aresetn,
-		output wire  s00_axis_tready,
-		input wire [C_S00_AXIS_TDATA_WIDTH-1 : 0] s00_axis_tdata,
-		input wire [(C_S00_AXIS_TDATA_WIDTH/8)-1 : 0] s00_axis_tstrb,
-		input wire  s00_axis_tlast,
-		input wire  s00_axis_tvalid,
-        input wire [63:0] tsf_runtime_val
-	);
+  // Ports of Axi Slave Bus Interface S00_AXIS to PS
+  input wire  s00_axis_aclk,
+  input wire  s00_axis_aresetn,
+  output wire  s00_axis_tready,
+  input wire [C_S00_AXIS_TDATA_WIDTH-1 : 0] s00_axis_tdata,
+  input wire [(C_S00_AXIS_TDATA_WIDTH/8)-1 : 0] s00_axis_tstrb,
+  input wire  s00_axis_tlast,
+  input wire  s00_axis_tvalid,
+  input wire [63:0] tsf_runtime_val
+);
 
 	function integer clogb2 (input integer bit_depth);                                   
     begin                                                                              
@@ -158,118 +158,118 @@ module tx_intf #
     
   localparam integer MAX_BIT_NUM_DMA_SYMBOL  = clogb2(MAX_NUM_DMA_SYMBOL);
 
-    wire slv_reg_rden;
-    wire [4:0] axi_araddr_core;
-    wire slv_reg_wren;
-    wire [4:0] axi_awaddr_core;
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg0; 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg1; // write arbitrary I/Q from this register port
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg2; // phy tx auto_start_mode and num_dma_symbol_th
-    // wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg3; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg4; // CTS duration for CTS-TO-SELF CTS-PROTECT TX
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg5; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg6; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg7; // TX arbitrary I/Q control
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg8; 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg9; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg10; 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg11; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg12; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg13; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg14; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg15; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg16; 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg17; // 
-    //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg18; 
-    //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg19;
-    // wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg20; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg21; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg22; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg23; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg24; 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg25; // 
-    wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg26; 
-    //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg27; // 
-    //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg28; // 
-    //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg29; // 
-    //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg30; // 
-    //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg31; // MSB 8 bits are used for version read out
-    
-    wire [(2*IQ_DATA_WIDTH-1) : 0] ant_data;
-    wire ant_data_valid;
-    
-    wire fulln_from_dac_to_duc;
+  wire slv_reg_rden;
+  wire [4:0] axi_araddr_core;
+  wire slv_reg_wren;
+  wire [4:0] axi_awaddr_core;
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg0; 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg1; // write arbitrary I/Q from this register port
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg2; // phy tx auto_start_mode and num_dma_symbol_th
+  // wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg3; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg4; // CTS duration for CTS-TO-SELF CTS-PROTECT TX
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg5; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg6; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg7; // TX arbitrary I/Q control
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg8; 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg9; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg10; 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg11; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg12; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg13; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg14; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg15; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg16; 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg17; // 
+  //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg18; 
+  //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg19;
+  // wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg20; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg21; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg22; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg23; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg24; 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg25; // 
+  wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg26; 
+  //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg27; // 
+  //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg28; // 
+  //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg29; // 
+  //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg30; // 
+  //wire [(C_S00_AXI_DATA_WIDTH-1):0] slv_reg31; // MSB 8 bits are used for version read out
+  
+  wire [(2*IQ_DATA_WIDTH-1) : 0] ant_data;
+  wire ant_data_valid;
+  
+  wire fulln_from_dac_to_duc;
 
-    wire [(2*IQ_DATA_WIDTH-1) : 0] wifi_iq_pack;
-    wire wifi_iq_valid;
-    wire wifi_iq_ready;
+  wire [(2*IQ_DATA_WIDTH-1) : 0] wifi_iq_pack;
+  wire wifi_iq_valid;
+  wire wifi_iq_ready;
   //While wifi_iq_ready-->iq_valid_for_check has "bad luck" phase to iq0_for_check (valid/strobe == 1 at the end of the sample stable period), 
   //the FPGA internal loopback in rx_intf (slv_reg3[8] controlled) will not work.
   //So, we delay the wifi_iq_ready on purpose by 1 clk to wifi_iq_ready_delay. (Need to dig further why the openofdm_rx does not work)
   wire wifi_iq_ready_delay;
 
-    wire [(C_S00_AXIS_TDATA_WIDTH-1):0] s_axis_data_to_acc;
-    wire tx_bit_intf_acc_ask_data_from_s_axis;
-    wire acc_ask_data_from_s_axis;
-    wire s_axis_emptyn_to_acc;
-    wire [(MAX_BIT_NUM_DMA_SYMBOL-1) : 0] s_axis_fifo_data_count0;
-    wire [(MAX_BIT_NUM_DMA_SYMBOL-1) : 0] s_axis_fifo_data_count1;
-    wire [(MAX_BIT_NUM_DMA_SYMBOL-1) : 0] s_axis_fifo_data_count2;
-    wire [(MAX_BIT_NUM_DMA_SYMBOL-1) : 0] s_axis_fifo_data_count3;
-    wire [1:0] tx_queue_idx;
-    wire [1:0] linux_prio;
-    wire [5:0] bd_wr_idx;
-    wire [5:0] pkt_cnt;
-    // wire [15:0] tx_pkt_num_dma_byte;
+  wire [(C_S00_AXIS_TDATA_WIDTH-1):0] s_axis_data_to_acc;
+  wire tx_bit_intf_acc_ask_data_from_s_axis;
+  wire acc_ask_data_from_s_axis;
+  wire s_axis_emptyn_to_acc;
+  wire [(MAX_BIT_NUM_DMA_SYMBOL-1) : 0] s_axis_fifo_data_count0;
+  wire [(MAX_BIT_NUM_DMA_SYMBOL-1) : 0] s_axis_fifo_data_count1;
+  wire [(MAX_BIT_NUM_DMA_SYMBOL-1) : 0] s_axis_fifo_data_count2;
+  wire [(MAX_BIT_NUM_DMA_SYMBOL-1) : 0] s_axis_fifo_data_count3;
+  wire [1:0] tx_queue_idx;
+  wire [1:0] linux_prio;
+  wire [5:0] bd_wr_idx;
+  wire [5:0] pkt_cnt;
+  // wire [15:0] tx_pkt_num_dma_byte;
 
-    wire [6:0] tx_config_fifo_data_count0;
-    wire [6:0] tx_config_fifo_data_count1;
-    wire [6:0] tx_config_fifo_data_count2;
-    wire [6:0] tx_config_fifo_data_count3;
+  wire [6:0] tx_config_fifo_data_count0;
+  wire [6:0] tx_config_fifo_data_count1;
+  wire [6:0] tx_config_fifo_data_count2;
+  wire [6:0] tx_config_fifo_data_count3;
 
-    wire [5:0] dac_intf_rd_data_count;
-    wire [5:0] dac_intf_wr_data_count;
-    wire [27:0] dac_intf_counter_1s;
-    wire [26:0] dac_intf_counter_rden_store;
-    wire [27:0] dac_intf_counter_dac;
-    wire [26:0] dac_intf_counter_wren_store;
-    
-    wire phy_tx_auto_start_mode;
-    wire [9:0] phy_tx_auto_start_num_dma_symbol_th;
-    
-    wire s_axis_recv_data_from_high;
-    
-    wire tx_itrpt_internal;
+  wire [5:0] dac_intf_rd_data_count;
+  wire [5:0] dac_intf_wr_data_count;
+  wire [27:0] dac_intf_counter_1s;
+  wire [26:0] dac_intf_counter_rden_store;
+  wire [27:0] dac_intf_counter_dac;
+  wire [26:0] dac_intf_counter_wren_store;
+  
+  wire phy_tx_auto_start_mode;
+  wire [9:0] phy_tx_auto_start_num_dma_symbol_th;
+  
+  wire s_axis_recv_data_from_high;
+  
+  wire tx_itrpt_internal;
 
-    wire [13:0] send_cts_toself_wait_sifs_top;
+  wire [13:0] send_cts_toself_wait_sifs_top;
 
-    assign iq0_for_check = wifi_iq_pack;
-    assign iq1_for_check = 0; //wifi_iq1_pack;
+  assign iq0_for_check = wifi_iq_pack;
+  assign iq1_for_check = 0; //wifi_iq1_pack;
   assign iq_valid_for_check = wifi_iq_ready_delay;
 
-    assign bram_addr_to_xpu = bram_addr;
-    assign send_cts_toself_wait_sifs_top = (band==1?slv_reg6[13:0]:slv_reg6[29:16]);
+  assign bram_addr_to_xpu = bram_addr;
+  assign send_cts_toself_wait_sifs_top = (band==1?slv_reg6[13:0]:slv_reg6[29:16]);
 
-	assign phy_tx_auto_start_mode = slv_reg2[3];
-	assign phy_tx_auto_start_num_dma_symbol_th = slv_reg2[13:4];
+  assign phy_tx_auto_start_mode = slv_reg2[3];
+  assign phy_tx_auto_start_num_dma_symbol_th = slv_reg2[13:4];
 
-    assign slv_reg21[0] = (s_axis_fifo_data_count0>slv_reg11[(MAX_BIT_NUM_DMA_SYMBOL-1):0]?1:0);
-    assign slv_reg21[1] = (s_axis_fifo_data_count1>slv_reg11[(MAX_BIT_NUM_DMA_SYMBOL-1):0]?1:0);
-    assign slv_reg21[2] = (s_axis_fifo_data_count2>slv_reg11[(MAX_BIT_NUM_DMA_SYMBOL-1):0]?1:0);
-    assign slv_reg21[3] = (s_axis_fifo_data_count3>slv_reg11[(MAX_BIT_NUM_DMA_SYMBOL-1):0]?1:0);
+  assign slv_reg21[0] = (s_axis_fifo_data_count0>slv_reg11[(MAX_BIT_NUM_DMA_SYMBOL-1):0]?1:0);
+  assign slv_reg21[1] = (s_axis_fifo_data_count1>slv_reg11[(MAX_BIT_NUM_DMA_SYMBOL-1):0]?1:0);
+  assign slv_reg21[2] = (s_axis_fifo_data_count2>slv_reg11[(MAX_BIT_NUM_DMA_SYMBOL-1):0]?1:0);
+  assign slv_reg21[3] = (s_axis_fifo_data_count3>slv_reg11[(MAX_BIT_NUM_DMA_SYMBOL-1):0]?1:0);
 
-    assign acc_ask_data_from_s_axis = tx_bit_intf_acc_ask_data_from_s_axis;
+  assign acc_ask_data_from_s_axis = tx_bit_intf_acc_ask_data_from_s_axis;
 
-    assign tx_itrpt = (slv_reg14[17]==0?(slv_reg14[8]?tx_itrpt_internal: (tx_itrpt_internal&(~ack_tx_flag)) ):0);
+  assign tx_itrpt = (slv_reg14[17]==0?(slv_reg14[8]?tx_itrpt_internal: (tx_itrpt_internal&(~ack_tx_flag)) ):0);
 
-    // assign slv_reg26[1:0] = tx_queue_idx;
-    // assign slv_reg26[13:2] = bd_wr_idx;
-    assign slv_reg26[6:0] = tx_config_fifo_data_count0;
-    assign slv_reg26[14:8] = tx_config_fifo_data_count1;
-    assign slv_reg26[22:16] = tx_config_fifo_data_count2;
-    assign slv_reg26[30:24] = tx_config_fifo_data_count3;
+  // assign slv_reg26[1:0] = tx_queue_idx;
+  // assign slv_reg26[13:2] = bd_wr_idx;
+  assign slv_reg26[6:0] = tx_config_fifo_data_count0;
+  assign slv_reg26[14:8] = tx_config_fifo_data_count1;
+  assign slv_reg26[22:16] = tx_config_fifo_data_count2;
+  assign slv_reg26[30:24] = tx_config_fifo_data_count3;
 
-    assign tx_queue_idx_to_xpu = tx_queue_idx;
+  assign tx_queue_idx_to_xpu = tx_queue_idx;
 
 `ifndef TX_INTF_DISCONNECT_LED
   edge_to_flip edge_to_flip_tx_itrpt_i (
